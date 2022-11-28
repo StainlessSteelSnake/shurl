9768b876c0f10f79bb1476f8cabe81a1a4e5b3ad
StainlessSteelSnake <stainlesssteelsnake@gmail.com>
2022-11-28 16:19:28 +0300

    Исправлен тест функции Start()

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -7,12 +7,9 @@ import (
 func TestStart(t *testing.T) {
 	tests := []struct {
 		name string
-	}{
-		{"Positive case"},
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Start()
 			t.Log("Started", tt.name)
 		})
 	}
